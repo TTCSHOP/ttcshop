6abb5c84b0a399bb0f5bae584bc91607cd72b1e9
nga <ttntrinhnga@gmail.com>
2020-04-30 18:22:22 +0700

    Update handler_order.php

--- order/handler_order.php
+++ order/handler_order.php
@@ -33,15 +33,21 @@
                     $get_cart = mysqli_query($connect,"SELECT * FROM cart WHERE user_id = $userId");// lấy thông tin từ giỏ hàng
                    
                     $num_cart = mysqli_num_rows($get_cart);// số sp có trong giỏ hàng
-                  
+                    
                     for($j=1; $j<=$num_cart;$j++){
                         $get_cart_array = mysqli_fetch_array($get_cart);
                         $product_id = $get_cart_array['product_id'];
-                        $amount = $get_cart_array['amount'];
+                        $amount = $get_cart_array['amount'];// lấy số lượng của sp có trong giỏ hàng
+                        $result_quantiyInStock = mysqli_query($connect,"SELECT * FROM products WHERE id = $product_id");// ktra sl tồn kho
+                        $quantity = mysqli_fetch_array($result_quantiyInStock);// lấy ra sl tồn kho
+
                         $totalMoney = $get_cart_array['totalMoney'];
                         
                         $insert_orderdetails = mysqli_query($connect,"INSERT INTO orderdetails(product_id,amount,totalMoney,orderID,status)
                         VALUES('$product_id','$amount','$totalMoney','$orderID',NOW())");
+                        // cập nhật thông tin số lượng sp tồn kho
+                        $quantity_remain = $quantity['quantityInStock'] - $amount;// số sp còn lại sau khi đặt hàng
+                        mysqli_query($connect,"UPDATE `products` SET `quantityInStock` = '$quantity_remain' WHERE id = $product_id");
                     }   
                     
                    
