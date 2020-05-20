637c081a9078eca162023c72e74efaad48ec3912
namtranpt00 <namtranpt00@users.noreply.github.com>
2020-05-20 22:43:00 +0700

    nam 20/5

--- phone/add_cart.php
+++ phone/add_cart.php
@@ -113,7 +113,7 @@ if (isset($_COOKIE['userId'])) {
                             </div>
                             <div style="margin-top: 10px; text-align: center; color: #3c3d41;float: left;width: 100%;">
                                 <a href="https://cellphones.com.vn/tra-gop/mpos" target="_blank">Trả góp 0% với thẻ tín dụng tại cửa hàng - Xem chi tiết </a><br>
-                                Gọi miễn phí: <b>1800.2097</b> 
+                                Gọi miễn phí: <b>1800.2097</b>
                             </div>
                         </div>
                     </div>
@@ -164,96 +164,43 @@ if (isset($_COOKIE['userId'])) {
                             <td style="width:200px">Pin:</td>
                             <td><?php echo $row_details['pin'] ?></td>
                         </tr>
+
                     </tbody>
                 </table>
-            </div>
+                <button type="button" class="btn border-info rounded-pill " data-toggle="modal" data-target="#thanks">
+                More
+                </button>
 
-
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-md-6">
-                    <div class="well well-sm">
-                        <div class="row">
-                            <div class="col-xs-12 col-md-6 text-center">
-                                <h1 class="rating-num">
-                                    4.0</h1>
-                                <div class="rating">
-                                    <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
-                                    </span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
-                                    </span><span class="glyphicon glyphicon-star-empty"></span>
-                                </div>
-                                <div>
-                                    <span class="glyphicon glyphicon-user"></span>1,050,008 total
-                                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="thanks" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Cảm ơn quý khách đã quan tâm</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
-                            <div class="col-xs-12 col-md-6">
-                                <div class="row rating-desc">
-                                    <div class="col-xs-3 col-md-3 text-right">
-                                        <span class="glyphicon glyphicon-star"></span>5
-                                    </div>
-                                    <div class="col-xs-8 col-md-9">
-                                        <div class="progress progress-striped">
-                                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                                                <span class="sr-only">80%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end 5 -->
-                                    <div class="col-xs-3 col-md-3 text-right">
-                                        <span class="glyphicon glyphicon-star"></span>4
-                                    </div>
-                                    <div class="col-xs-8 col-md-9">
-                                        <div class="progress">
-                                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                                <span class="sr-only">60%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end 4 -->
-                                    <div class="col-xs-3 col-md-3 text-right">
-                                        <span class="glyphicon glyphicon-star"></span>3
-                                    </div>
-                                    <div class="col-xs-8 col-md-9">
-                                        <div class="progress">
-                                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                                <span class="sr-only">40%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end 3 -->
-                                    <div class="col-xs-3 col-md-3 text-right">
-                                        <span class="glyphicon glyphicon-star"></span>2
-                                    </div>
-                                    <div class="col-xs-8 col-md-9">
-                                        <div class="progress">
-                                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                                                <span class="sr-only">20%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end 2 -->
-                                    <div class="col-xs-3 col-md-3 text-right">
-                                        <span class="glyphicon glyphicon-star"></span>1
-                                    </div>
-                                    <div class="col-xs-8 col-md-9">
-                                        <div class="progress">
-                                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
-                                                <span class="sr-only">15%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end 1 -->
-                                </div>
-                                <!-- end row -->
+                            <div class="modal-body">
+                                TTC Shop cam kết mọi sản phẩm của shop đều là hàng chính hãng mới 100% new seal. Tất cả đều được shop bảo hành ít nhất 12 tháng,
+                                với những mẫu cá biệt được bảo hành đến 18 tháng. TTC mua sự hài lòng của quý khách, cảm ơn quý khách đã ghé qua trang web của cửa hàng.
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+
         </div>
 
+
         <?php
+
         include('../includes/foot.php');
         ?>
     </body>
