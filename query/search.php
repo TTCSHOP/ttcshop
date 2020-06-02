d4f41578c1f2afc203969db1a772e95a6256be32
ngasophie <57297730+ngasophie@users.noreply.github.com>
2020-06-02 19:46:19 +0700

    Update search.php

--- query/search.php
+++ query/search.php
@@ -7,24 +7,23 @@ if (isset($_POST['search'])) {
         header("Location:./notFound.php");
         exit();
     } else {
-        $result = mysqli_query($connect, "
+        $result_pr = mysqli_query($connect, "
             SELECT * FROM products WHERE  (name LIKE '%$searchtxt%'
                 OR category LIKE '%$searchtxt%') AND quantityInStock>=0;
                  ");
-        if ($result) {
-            $num = mysqli_num_rows($result);
-            if ($num > 0) {
+        if ($result_pr) {
+            $num = mysqli_num_rows($result_pr);
+            if ($num > 0 && $num < 10) {
                 include('../includes/head.php');
                 require('../includes/advertisement.php');
-               
                 echo '<div class=" container mt-5 " id="content">';
                 echo '<div class="content-card d-flex justify-content-between flex-wrap">';
-                if($num >8) $num =8;
+                
                 for ($j = 1; $j <= $num; $j++) {
                     // $result = mysqli_query($connect, "SELECT * FROM products WHERE id=$i");
 
-                    $row[$j] =  mysqli_fetch_array($result);
-
+                    $row[$j] =  mysqli_fetch_array($result_pr);
+                   
                     echo
                         '<div class="card mb-3 mr-ml-1" style="width: 270px;">
                         <a  href="../phone/add_cart.php?buynow=' . $row[$j][0] . '">
@@ -43,12 +42,13 @@ if (isset($_POST['search'])) {
                             </div>';
                 }
                 echo '</div></div>';
+               
             } 
-                    else {
+        }
+            else {
                 header("Location:./notFound.php");
                 exit();
             }
-        }
             include('../includes/foot.php');
         // }
     }
